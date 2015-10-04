156d1013ebcc4279eec278e2006307fb7e68e69c
Amanda Carlton <acarlton27@gmail.com>
2015-10-04 17:20:12 -0600

    clean code

--- routes/index.js
+++ routes/index.js
@@ -13,27 +13,19 @@ router.get('/', function(req, res, next) {
 router.post('/reddit', function (req,res,next) {
   userCollection.findOne({_id:req.body.user_id}).then(function (response) {
   var testcats = response.interest;
-  console.log(testcats);
   var random = Math.floor(Math.random() * (testcats.length));
-  console.log(testcats[random]);
   unirest.get('https://www.reddit.com/r/'+testcats[random]+'.json?')
   .end(function (response) {
-    console.log(response.body);
     var randomchild = Math.floor(Math.random() * (response.body.data.children.length));
-    console.log(randomchild);
     var info = response.body.data.children[randomchild].data;
-    console.log(info.domain);
     if(info.domain === "youtube.com" || info.domain === "twitter.com" || info.domain === "vine.co" || info.domain === "m.youtube.com"){
-      console.log(info.url);
      unirest.get('http://api.embed.ly/1/oembed?key=:'+process.env.EMBEDLY_API+'&url='+info.url)
      .end(function (tube) {
-       console.log(tube.body);
        var regex = /src="(.+?)"/;
        var matches = regex.exec(tube.body.html);
        res.json(matches[1]);
      });
     }else{
-      console.log(info.url);
     res.json(info.url);
     }
   });
@@ -41,13 +33,8 @@ router.post('/reddit', function (req,res,next) {
 });
 
 router.post('/insert', function (req, res, next) {
-  console.log("here");
-  console.log(req.body.user_id);
-  console.log(req.body);
-  // var category = req.body.interest;
   var category={};
   category[req.body.interest] = {liked:[], disliked:[], time: {timeliked:[], timedisliked:[], timeall:[]}};
-  console.log(category);
   userCollection.update({_id:req.body.user_id}, { $set: category});
   userCollection.update({_id:req.body.user_id}, {$push:{interest:req.body.interest}});
 
@@ -55,7 +42,6 @@ router.post('/insert', function (req, res, next) {
 });
 
 router.post('/insertuser', function (req, res, next) {
-  console.log(req.body);
   var hash = bcrypt.hashSync(req.body.password, 8);
   userCollection.insert({name:req.body.name, email:req.body.email, password:hash, interest:[]}).then(function (response) {
    res.json(response);
