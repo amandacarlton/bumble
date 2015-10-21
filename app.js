725c14a6c2adae5bfcccf97e7f8854ac39960f4f
Amanda Carlton <acarlton27@gmail.com>
2015-10-21 08:58:53 -0600

    new branch

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ var users = require('./routes/users');
 
 var app = express();
 
-var forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    return next();
- };
-
- app.use(forceSsl);
+// var forceSsl = function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//         return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//     }
+//     return next();
+//  };
+//
+//  app.use(forceSsl);
 
 // app.use(enforce.HTTPS(true));
 
