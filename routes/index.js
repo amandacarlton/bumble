ecf54b511e017c2243d7441edaa75108730811fa
Amanda Carlton <acarlton27@gmail.com>
2015-10-20 20:33:25 -0600

    heroku http removal

--- routes/index.js
+++ routes/index.js
@@ -20,10 +20,10 @@ router.post('/reddit', function (req,res,next) {
   console.log(req.body.user_id);
   var testcats;
   if(req.body.user_id === undefined){
-    testcats = ['puppies', 'aww', 'food', 'news', 'nottheonion', 'gadgets', 'EarthPorn', 'dataisbeautiful', 'science'];
+    testcats = ['puppies', 'aww', 'food', 'news', 'nottheonion', 'gadgets', 'EarthPorn', 'dataisbeautiful', 'science', 'gifs'];
     var random = Math.floor(Math.random() * (testcats.length));
     var categoryChosen = (testcats[random]);
-    unirest.get('https://www.reddit.com/r/'+testcats[random]+'.json?')
+    unirest.get('//www.reddit.com/r/'+testcats[random]+'.json?')
     .end(function (response) {
       function security(item){
         return (item.data.domain != 'google.com' && item.data.domain != 'twitter.com' && item.data.domain.slice(0,5) != 'self.' && item.data.thumbnail != 'nsfw' && item.data.thumbnail != 'NSFW');
@@ -33,7 +33,7 @@ router.post('/reddit', function (req,res,next) {
       var info = filteredResponse[randomchild].data;
       info.category = categoryChosen;
       if(info.domain === "youtube.com" || info.domain === "twitter.com" || info.domain === "vine.co" || info.domain === "m.youtube.com" || info.domain === "google.com" || info.domain === "en-maktoob.news.yahoo.com" || info.domain === 'flickr.com' || info.domain === 'youtu.be'){
-        unirest.get('http://api.embed.ly/1/oembed?key=:'+process.env.EMBEDLY_API+'&url='+info.url)
+        unirest.get('//api.embed.ly/1/oembed?key=:'+process.env.EMBEDLY_API+'&url='+info.url)
         .end(function (tube) {
           var regex = /src="(.+?)"/;
           var matches = regex.exec(tube.body.html);
@@ -50,7 +50,7 @@ router.post('/reddit', function (req,res,next) {
       var random = Math.floor(Math.random() * (testcats.length));
       var categoryChosen = (testcats[random]);
       console.log('Category',categoryChosen);
-      unirest.get('https://www.reddit.com/r/'+testcats[random]+'.json?')
+      unirest.get('//www.reddit.com/r/'+testcats[random]+'.json?')
       .end(function (response) {
         function security(item){
           return (item.data.domain != 'google.com' && item.data.domain != 'twitter.com' && item.data.domain.slice(0,5) != 'self.' && item.data.thumbnail != 'nsfw' && item.data.thumbnail != 'NSFW');
@@ -60,7 +60,7 @@ router.post('/reddit', function (req,res,next) {
         var info = filteredResponse[randomchild].data;
         info.category = categoryChosen;
         if(info.domain === "youtube.com" || info.domain === "twitter.com" || info.domain === "vine.co" || info.domain === "m.youtube.com" || info.domain === "google.com" || info.domain === "en-maktoob.news.yahoo.com" || info.domain === 'flickr.com' || info.domain === 'youtu.be'){
-          unirest.get('http://api.embed.ly/1/oembed?key=:'+process.env.EMBEDLY_API+'&url='+info.url)
+          unirest.get('//api.embed.ly/1/oembed?key=:'+process.env.EMBEDLY_API+'&url='+info.url)
           .end(function (tube) {
             var regex = /src="(.+?)"/;
             var matches = regex.exec(tube.body.html);
@@ -79,7 +79,7 @@ router.post('/reddit', function (req,res,next) {
 
 
 router.get("/reddittrend", function (req, res, next) {
-  unirest.get("https://www.reddit.com/.json")
+  unirest.get("//www.reddit.com/.json")
   .end(function (response) {
 
       console.log(response.body.data.children[3].data);
@@ -149,7 +149,7 @@ router.post("/subscriber", function (req, res, next) {
   for (var i = 0; i < length; i++) {
     var last = req.body.category[length-1];
     var catname = req.body.category[i];
-    unirest.get('https://www.reddit.com/r/'+req.body.category[i]+'/about.json?')
+    unirest.get('//www.reddit.com/r/'+req.body.category[i]+'/about.json?')
     .end(function (response) {
       subscriberinfo[response.body.data.display_name] = response.body.data.subscribers;
       counter ++;
@@ -167,7 +167,7 @@ router.post("/subscriberstate", function (req, res, next) {
   for (var i = 0; i < length; i++) {
     var last = req.body.category[length-1];
     var catname = req.body.category[i];
-    unirest.get('https://www.reddit.com/r/'+req.body.category[i]+'/about.json?')
+    unirest.get('//www.reddit.com/r/'+req.body.category[i]+'/about.json?')
     .end(function (response) {
       subscriberinfo[response.body.data.display_name] = response.body.data.subscribers;
       counter ++;
